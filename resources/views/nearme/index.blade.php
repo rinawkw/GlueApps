b2b723eb157a006387a23c1f9246954ccd5fa2a7
Rina Wijaya <rinawkw@gmail.com>
2018-04-01 16:41:17 +0800

    info marker

--- resources/views/nearme/index.blade.php
+++ resources/views/nearme/index.blade.php
@@ -5,39 +5,45 @@
 <div class="boxed active">
     @include('layouts.topbar')
     @include('layouts.mainmenu')
-
-			<!-- main content -->
-			<section class="ot-section-a">
-                <!-- container -->
-                
+    <!-- main content -->
+        <section class="ot-section-a">
+				<!-- container -->
 				<div class="container">
-                <div class="ot-module">
-                    <!-- Modal -->
-                    <div style="z-index: 1050;" class="modal fade" id="myModal" role="dialog">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            </div>
-                        </div>
-                    </div>
-                    <!-- classic grid posts section -->
-                    <h4 class="section-title"><span>Lokasi Anggota GenBI</span>GenBI Near Me</h4>
 					<div class="row">
-						<div class="col-md-12">
+                    <h4 class="section-title"><span>Lokasi Anggota GenBI</span>GenBI Near Me</h4>
+						<div class="col-md-8">
 							<div class="content ot-article">
-								<div style="border:0.5rem solid #51aee0;" id="mymap" class="iframe-rwd embed-container maps">
-                                </div>
+                                
+                                <div style="border:0.5rem solid #51aee0;" id="mymap" class="iframe-rwd embed-container maps">
+                                </div>    							
 							</div>
 							<!-- end content -->
 						</div>
-						
+						<div class="col-md-4">
+							<aside class="sidebar">
+								<!-- widget articles section -->
+								<!-- <div class="widget-container"> -->
+                                <div class="content ot-article">
+                                    <div class="post-image" style="margin:2rem">
+										<img id="pic"  src="{{session('data.user_foto')}}" alt="">
+                                    </div>
+                                    <div class="list-post">
+									    <h4 style="text-align:center"class="section-title">
+                                            <a id="profile" href="profile"><b id="demo">{{session('data.user_nama')}}</b></a>
+                                        </h4>	
+                                    </div>
+                                </div>
+								<!-- end widget articles section -->
+							</aside>
+						</div>
+						<!-- col-md-4 -->
 					</div>
 					<!-- row -->
-                    <!-- end main content -->
-                                    </div>
+					<!-- end main content -->
 				</div>
 				<!-- container -->
 			</section>
-            @include('layouts.footer')
+      @include('layouts.footer')
 </div>
 
 
@@ -52,14 +58,15 @@ var mymap = new GMaps({
   lng: locMe[0].user_lng,
   zoom:15
 });
-
+var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
 var marker = mymap.addMarker({
     lat: locMe[0].user_lat,
     lng: locMe[0].user_lng,
     title: "YOU",
     icon: "images/me.png",
-    click: function() {
-        $("#myModal").modal('show');
+    click: function(e) {
+        $('#demo').text(locMe[0].user_nama);
+        $("#pic").attr({ src: locMe[0].user_foto});
     }    
 });
 
@@ -74,7 +81,9 @@ $.each( locOthers, function( index, value ){
         title: value.user_nama,
         icon: "images/other.png",
         click: function(e) {
-            $("#myModal").modal('show');
+            $('#demo').text(value.user_nama);
+            $("#pic").attr({ src: value.user_foto});
+            $("#profile").attr("href", "profile/".value.user_nrp);
         }
     });
 });
